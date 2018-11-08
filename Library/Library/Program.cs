31733481e80d2e276fb9752cb14035f2de8c3f02
Anders <anders@nntq.net>
2018-11-08 10:34:33 +0100

    Added functionality
    Created a main loop and some input handling so that the user can run functions.
    Added a Loaner class so that loaners can loan books from the library.
    Added a DateTime object variable to the Book and Library classes to that loan times can be tracked.
    Added a function for displaying all books in the console, called "DisplayAllBooks."
    Added a WIP function called "LoanBook" used for loaning a book from the library.
    Added a function for retrieving loan time from a book.

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -7,21 +7,52 @@ namespace Library
 	{
 		static void Main(string[] args)
 		{
+			string[] options = {"Display a list of all books","Add a new book to the library","Loan a book"};
+			bool isRunning = true;
 			Library lib = new Library();
+
+			while(isRunning)
+			{
+				for(int i = 0; i < options.Length; i += 1)
+				{
+					Console.WriteLine(i+1 + ") " + options[i]);
+				}
+
+				switch (Console.ReadLine()[0])
+				{
+					case '1':
+						lib.DisplayAllBooks();
+						break;
+
+					case '2':
+						lib.AddBookToLibrary();
+						break;
+
+					case '3':
+						
+						break;
+				}
+			}
 		}
+	}
+
+	class Loaner
+	{
+		readonly List<Book> loanedBooks = new List<Book>();
+		readonly string loanerName;
 	}
 
 	class Book
 	{
 		readonly string title;
 		readonly string author;
-		readonly bool isBorrowed;
+		readonly DateTime timeLoaned;
+		readonly bool isLoaned;
 
 		public Book(string _title, string _author)
 		{
 			title = _title;
 			author = _author;
-			isBorrowed = false;
 		}
 
 		public string GetTitle()
@@ -34,19 +65,44 @@ namespace Library
 			return author;
 		}
 
-		public bool IsBorrowed()
+		public DateTime GetTimeLoaned()
 		{
-			return isBorrowed;
+			return timeLoaned;
+		}
+
+		public bool IsLoanedOut()
+		{
+			return isLoaned;
 		}
 	}	
 
 	class Library
 	{
 		readonly List<Book> books = new List<Book>();
+		readonly DateTime maxLoanTime = new DateTime(0);
+
+		public void DisplayAllBooks()
+		{
+			string loanedStat;
+			for(int i = 0; i < books.Count; i += 1)
+			{
+				if(books[i].IsLoanedOut())
+					loanedStat = "Yes";
+				else
+					loanedStat = "No";
+
+				Console.WriteLine($"Title: {books[i].GetTitle()}\nAuthor: {books[i].GetAuthor()}\nLoaned out: {loanedStat}");
+			}
+		}
+
+		public Book LoanBook()
+		{
+			return new Book("", "");
+		}
 
 		/// <summary>
-		/// Retrieves a book based on whether the parameter "title" matches the book's
-		/// "title" variable.
+		/// Matches the input title and author to all books
+		/// in the "books" list until it finds a match
 		/// </summary>
 		/// <param name="title">The title to be used for searching</param>
 		/// <returns>Returns an object of type Book, null if no match is found.</returns>
@@ -73,7 +129,6 @@ namespace Library
 			}
 			else
 				Console.WriteLine("A book with that title and author is already in the library.");
-
 		}
 	}
 }
