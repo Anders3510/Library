38548c8fc67bed7e51ef6e9f1ae3e41c7cb0a2c1
Anders <anders@nntq.net>
2018-11-08 08:45:16 +0100

    Error handling and functionality
    Added a new variable to the Book class, expanded the GetBookByTitle function to also operate by title and it has been renamed accordingly.
    Error handling as been added to AddBookToLibrary so that books with the same title but different authors can exist, but books which have identical authors and titles cannot.

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -14,17 +14,24 @@ namespace Library
 	class Book
 	{
 		readonly string title;
+		readonly string author;
 		readonly bool isBorrowed;
 
-		public Book(string _title)
+		public Book(string _title, string _author)
 		{
 			title = _title;
+			author = _author;
 			isBorrowed = false;
 		}
 
 		public string GetTitle()
 		{
 			return title;
+		}
+
+		public string GetAuthor()
+		{
+			return author;
 		}
 
 		public bool IsBorrowed()
@@ -43,10 +50,10 @@ namespace Library
 		/// </summary>
 		/// <param name="title">The title to be used for searching</param>
 		/// <returns>Returns an object of type Book, null if no match is found.</returns>
-		private Book GetBookByTitle(string title)
+		private Book GetBookByTitleAndAuthor(string title, string author)
 		{
 			for(int i = 0; i < books.Count; i += 1)
-				if(books[i].GetTitle() == title)
+				if(books[i].GetTitle() == title && books[i].GetAuthor() == author)
 					return books[i];
 
 			return null;
@@ -55,8 +62,18 @@ namespace Library
 		public void AddBookToLibrary()
 		{
 			Console.WriteLine("Please enter the title of the book you would like to add.");
-			books.Add(new Book(Console.ReadLine()));
-			Console.WriteLine("Your book has been added to the library.");
+			string tempTitle = Console.ReadLine();
+			Console.WriteLine("Who authored the book?");
+			string tempAuthor = Console.ReadLine();
+			
+			if(GetBookByTitleAndAuthor(tempTitle, tempAuthor) == null)
+			{
+				books.Add(new Book(tempTitle, tempAuthor));
+				Console.WriteLine("Book has been added to the library.");
+			}
+			else
+				Console.WriteLine("A book with that title and author is already in the library.");
+
 		}
 	}
 }
