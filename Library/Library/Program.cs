9e3d00c0753bfcb766122dc25f9fbb9d0fa1798f
Anders <anders@nntq.net>
2018-11-08 08:24:06 +0100

    Added classes and simple functions
    Created the Library and Book classes, and created function for retrieving a book from the library by title, and a function for adding books to the library, for the first class, and functions for retrieving object variables for the Book class.

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Library
 {
@@ -6,9 +7,56 @@ namespace Library
 	{
 		static void Main(string[] args)
 		{
-			
+			Library lib = new Library();
 		}
 	}
 
+	class Book
+	{
+		readonly string title;
+		readonly bool isBorrowed;
 
+		public Book(string _title)
+		{
+			title = _title;
+			isBorrowed = false;
+		}
+
+		public string GetTitle()
+		{
+			return title;
+		}
+
+		public bool IsBorrowed()
+		{
+			return isBorrowed;
+		}
+	}	
+
+	class Library
+	{
+		readonly List<Book> books = new List<Book>();
+
+		/// <summary>
+		/// Retrieves a book based on whether the parameter "title" matches the book's
+		/// "title" variable.
+		/// </summary>
+		/// <param name="title">The title to be used for searching</param>
+		/// <returns>Returns an object of type Book, null if no match is found.</returns>
+		private Book GetBookByTitle(string title)
+		{
+			for(int i = 0; i < books.Count; i += 1)
+				if(books[i].GetTitle() == title)
+					return books[i];
+
+			return null;
+		}
+
+		public void AddBookToLibrary()
+		{
+			Console.WriteLine("Please enter the title of the book you would like to add.");
+			books.Add(new Book(Console.ReadLine()));
+			Console.WriteLine("Your book has been added to the library.");
+		}
+	}
 }
