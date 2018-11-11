84d314ad65abde212caf277b49662fe4af1abf67
Anders <anders@nntq.net>
2018-11-11 18:38:31 +0100

    Added functionality regarding loaning books
    A function is now implemented that allows the user to loan books and register them to a specific loaner from the loaners List.

--- Library/Library/Program.cs
+++ Library/Library/Program.cs
@@ -7,15 +7,15 @@ namespace Library
 	{
 		static void Main(string[] args)
 		{
-			string[] options = {"Display a list of all books","Add a new book to the library","Loan a book"};
+			string[] options = { "Display a list of all books", "Add a new book to the library", "Loan a book" };
 			bool isRunning = true;
 			Library lib = new Library();
 
-			while(isRunning)
+			while (isRunning)
 			{
-				for(int i = 0; i < options.Length; i += 1)
+				for (int i = 0; i < options.Length; i += 1)
 				{
-					Console.WriteLine(i+1 + ") " + options[i]);
+					Console.WriteLine(i + 1 + ") " + options[i]);
 				}
 
 				switch (Console.ReadLine()[0])
@@ -29,7 +29,7 @@ namespace Library
 						break;
 
 					case '3':
-						
+						lib.LoanBook();
 						break;
 				}
 			}
@@ -40,14 +40,29 @@ namespace Library
 	{
 		readonly List<Book> loanedBooks = new List<Book>();
 		readonly string loanerName;
+
+		public Loaner(string _loanerName)
+		{
+			loanerName = _loanerName;
+		}
+
+		public string GetName()
+		{
+			return loanerName;
+		}
+
+		public List<Book> GetLoanedBooks()
+		{
+			return loanedBooks;
+		}
 	}
 
 	class Book
 	{
-		readonly string title;
-		readonly string author;
-		readonly DateTime timeLoaned;
-		readonly bool isLoaned;
+		public readonly string title;
+		public readonly string author;
+		public readonly DateTime timeLoaned;
+		public bool isLoaned;
 
 		public Book(string _title, string _author)
 		{
@@ -74,30 +89,81 @@ namespace Library
 		{
 			return isLoaned;
 		}
-	}	
+	}
 
 	class Library
 	{
 		readonly List<Book> books = new List<Book>();
+		readonly List<Loaner> loaners = new List<Loaner>();
 		readonly DateTime maxLoanTime = new DateTime(0);
+
+		public Library()
+		{
+			loaners.Add(new Loaner("Gert"));
+			loaners.Add(new Loaner("Flamingo"));
+			loaners.Add(new Loaner("Henning"));
+			
+		}
 
 		public void DisplayAllBooks()
 		{
 			string loanedStat;
-			for(int i = 0; i < books.Count; i += 1)
+			int textPad = 20;
+			Console.WriteLine("  " + "Title".PadRight(textPad) + "Author".PadRight(textPad) + "Loaned out");
+
+			for (int i = 0; i < books.Count; i += 1)
 			{
-				if(books[i].IsLoanedOut())
+				if (books[i].IsLoanedOut())
 					loanedStat = "Yes";
 				else
 					loanedStat = "No";
 
-				Console.WriteLine($"Title: {books[i].GetTitle()}\nAuthor: {books[i].GetAuthor()}\nLoaned out: {loanedStat}");
+				Console.WriteLine(i + 1 + " " + books[i].GetTitle().PadRight(textPad) + books[i].GetAuthor().PadRight(textPad) + loanedStat);
 			}
 		}
 
-		public Book LoanBook()
+		public void LoanBook()
 		{
-			return new Book("", "");
+			string tempName;
+			Console.WriteLine("Enter the name of the loaner to register the loan to.");
+			tempName = Console.ReadLine();
+			if (GetLoanerByName(tempName) != null)
+			{
+				DisplayAllBooks();
+				Console.WriteLine("Please select which book you would like to loan.");
+				string input = Console.ReadLine();
+				int result;
+
+				while (!int.TryParse(input, out result))
+				{
+					Console.WriteLine("Please write a number.");
+					input = Console.ReadLine();
+				}
+
+				if (result > books.Count)
+					Console.WriteLine("Something went wrong, please try again.");
+				else
+				{
+					if (!books[result - 1].IsLoanedOut())
+					{
+						GetLoanerByName(tempName).GetLoanedBooks().Add(books[result - 1]);
+						books[result - 1].isLoaned = true;
+						Console.WriteLine("The book has been loaned sucessfully.");
+					}
+					else
+						Console.WriteLine("That book is already loaned out.");
+				}
+			}
+			else Console.WriteLine("No loaner has that name");
+		}
+
+		private Loaner GetLoanerByName(string name)
+		{
+			for (int i = 0; i < loaners.Count; i += 1)
+				if (loaners[i].GetName() == name)
+					return loaners[i];
+
+			return null;
 		}
 
 		/// <summary>
@@ -106,10 +172,19 @@ namespace Library
 		/// </summary>
 		/// <param name="title">The title to be used for searching</param>
 		/// <returns>Returns an object of type Book, null if no match is found.</returns>
-		private Book GetBookByTitleAndAuthor(string title, string author)
+		private Book GetBookByTitle(string title, string author)
 		{
-			for(int i = 0; i < books.Count; i += 1)
-				if(books[i].GetTitle() == title && books[i].GetAuthor() == author)
+			for (int i = 0; i < books.Count; i += 1)
+				if (books[i].GetTitle() == title && books[i].GetAuthor() == author)
+					return books[i];
+
+			return null;
+		}
+
+		private Book GetBookByTitle(string title)
+		{
+			for (int i = 0; i < books.Count; i += 1)
+				if (books[i].GetTitle() == title)
 					return books[i];
 
 			return null;
@@ -118,11 +193,23 @@ namespace Library
 		public void AddBookToLibrary()
 		{
 			Console.WriteLine("Please enter the title of the book you would like to add.");
-			string tempTitle = Console.ReadLine();
+			string tempTitle = Console.ReadLine().Trim();
+			while (tempTitle == "")
+			{
+				Console.WriteLine("Please write a title.");
+				tempTitle = Console.ReadLine().Trim();
+			}
+
 			Console.WriteLine("Who authored the book?");
-			string tempAuthor = Console.ReadLine();
-			
-			if(GetBookByTitleAndAuthor(tempTitle, tempAuthor) == null)
+			string tempAuthor = Console.ReadLine().Trim();
+			while (tempTitle == "")
+			{
+				Console.WriteLine("Please write a title.");
+				tempTitle = Console.ReadLine().Trim();
+			}
+
+
+			if (GetBookByTitle(tempTitle, tempAuthor) == null)
 			{
 				books.Add(new Book(tempTitle, tempAuthor));
 				Console.WriteLine("Book has been added to the library.");
@@ -130,5 +217,7 @@ namespace Library
 			else
 				Console.WriteLine("A book with that title and author is already in the library.");
 		}
+
+
 	}
 }
